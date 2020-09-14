6cd0f0606e201731728bbc91e34a17902e4dab11
Zbr1920410104 <765451419@qq.com>
2020-09-14 15:36:06 +0800

    feat(添加index三个页面的下页边距):

--- src/pages/index/measure/index.tsx
+++ src/pages/index/measure/index.tsx
@@ -5,6 +5,8 @@ import { useDispatch } from '@tarojs/redux';
 import { changeMeasureType } from '../../../actions/measure';
 // 组件
 import Preview from './preview';
+import './measure.css';
+
 // #region 书写注意
 //
 // 目前 typescript 版本还无法在装饰器模式下将 Props 注入到 Taro.Component 中的 props 属性
@@ -42,7 +44,7 @@ const Measure = () => {
   }, [tabCur]);
 
   return (
-    <View className="index">
+    <View className="measrure-box">
       <AtTabBar
         tabList={[{ title: '尿酸单项' }, { title: '三联检测' }]}
         onClick={(e) => {
